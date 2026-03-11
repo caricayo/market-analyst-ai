702dd83c2a18528557a211c551ad37e208a267f0
caricayo <christianaricayos@gmail.com>
2026-03-10 21:30:03 -1000

    Remove bot tables from Supabase schema

---
+++ supabase/migrations/20260311073000_remove_trading_bot_tables.sql
@@ -0,0 +1,7 @@
+drop table if exists public.bot_events cascade;
+drop table if exists public.simulation_results cascade;
+drop table if exists public.kill_switch_log cascade;
+drop table if exists public.heartbeats cascade;
+drop table if exists public.model_versions cascade;
+drop table if exists public.daily_stats cascade;
+drop table if exists public.trades cascade;
